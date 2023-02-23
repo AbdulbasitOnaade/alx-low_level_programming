e414be745a0d4152d63298688ba42c71e69c5dc4
AbdulbasitOnaade <ethdamilee17@gmail.com>
2023-02-22 18:00:53 -0800

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * sum_multiples - Computes and prints the sum of all the multiples of 3 or 5
+ * below 1024.
+ *
+ * Return: void.
+ */
+
+void sum_multiples(void)
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+}
